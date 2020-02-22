2b5635e2b61129ba0ca232af2f9f153af28412f5
M1048135 <M1048135@b2ml28169.mindtree.com>
2020-02-22 11:21:37 +0530

    Initial SpringBoot Checked in

---
+++ juteproduct/src/main/java/com/juteproduct/JuteProductApplication.java
@@ -0,0 +1,13 @@
+package com.juteproduct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JuteProductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JuteProductApplication.class, args);
+	}
+
+}
---
+++ juteproduct/src/test/java/com/juteproduct/JuteProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juteproduct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JuteProductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
